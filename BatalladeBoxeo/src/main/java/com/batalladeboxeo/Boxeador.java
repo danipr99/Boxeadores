ac44974df90b7c50d66b9eb9770a943852297767
danipr99 <dpdanipr@gmail.com>
2021-11-11 18:45:05 +0100

    Primer commit de verdad
    Todas las bases del proyecto asentadas. Punto de inicio del proyecto

---
+++ BatalladeBoxeo/src/main/java/com/batalladeboxeo/Boxeador.java
@@ -0,0 +1,44 @@
+package com.batalladeboxeo;
+
+import java.util.Random;
+
+/**
+ * @author Santiago Faci
+ * @version curso 2014-2015
+ */
+public class Boxeador extends Thread {
+
+	private String nombre;
+	private Ring ring;
+	private int golpes;
+	
+	public Boxeador(String nombre, Ring ring) {
+		
+		this.nombre = nombre;
+		this.ring = ring;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public int getGolpes() {
+		return golpes;
+	}
+	
+	public void pegar() {
+		golpes++;
+	}
+	
+	@Override
+	public void run() {
+		
+		while (ring.getGolpes() < 100) {
+			
+			ring.pegar(this);
+			try {
+				Thread.sleep(new Random().nextInt(500));
+			} catch (InterruptedException ie) {}
+		}
+	}
+}
